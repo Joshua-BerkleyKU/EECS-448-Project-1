0bdf2faf6c0fff6e9603830044fe06d5a054bc1a
Trey Werr <treywerr@ku.edu>
2020-09-07 14:08:19 -0500

    Modify ShipInterface
    Add const modifier to method declarations where appropriate.

--- ShipInterface.h
+++ ShipInterface.h
@@ -17,28 +17,28 @@ class ShipInterface {
 		/**
 		 * @return the length of the Ship
 		 */
-		virtual int getLength()=0;
+		virtual int getLength() const=0;
 
 		/**
 		 * Checks if the given coordinate is a hit on this Ship.
 		 * @return true if it is a hit, false otherwise
 		 */
-		virtual bool isHit(/* The coords to check */)=0;
+		virtual bool isHit(/* The coords to check */) const=0;
 
 		/**
 		 * @return the number of hits on this Ship
 		 */
-		virtual int numHits()=0;
+		virtual int numHits() const=0;
 
 		/**
 		 * @return true if all the ship's spaces have been hit, false otherwise
 		 */
-		virtual bool isSunk()=0;
+		virtual bool isSunk() const=0;
 		
 		/**
 		 * @return the coordinates the Ship occupies
 		 */
-		virtual std::string getLocation()=0;
+		virtual std::string getLocation() const=0;
 	
 };
 
